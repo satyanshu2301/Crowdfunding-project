01b8e37197d8e7175dec39287abb882f77fca947
Satyanshu Kesharwani <satyanshu2301@gmail.com>
2023-08-08 12:35:13 +0530

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains:  ['ipfs.infura.io', 'th.bing.com', 'thesaviours.infura-ipfs.io']
+   }
+}
+
+
+module.exports = nextConfig
